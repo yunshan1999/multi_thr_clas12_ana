7e6fa02a6edeac4f9e87cc77dda3093e8a424f03
yunshan1999 <62414952+yunshan1999@users.noreply.github.com>
2020-03-25 21:14:38 -0400

    Update branches.hpp

--- branches.hpp
+++ branches.hpp
@@ -12,7 +12,17 @@ class Branches12 {
   std::shared_ptr<TChain> _tree;
   
   Float_t _trigger;
+  v_float _ec_tot_energy;
+  v_float _ec_pcal_energy;
+  v_float _ec_ecin_energy;
+  v_float _ec_ecout_energy;
+  v_float _sc_ftof_1a_energy;
+  v_float _sc_ftof_1b_energy;
+  v_float _sc_ftof_2_energy;
+  v_float _sc_ctof_energy;
+  v_float _sc_cnd_energy;
   v_float _ft_cal_energy;
+  v_float _ft_hodo_energy; 
   
  public:
   Branches12(){};
@@ -20,6 +30,17 @@ class Branches12 {
   ~Branches12(){};
   void init();
   int trigger();
+  float ec_tot_energy(int i);
+  float ec_pcal_energy(int i);
+  float ec_ecin_energy(int i);
+  float ec_ecout_energy(int i);
+  float sc_ftof_1a_energy(int i);
+  float sc_ftof_1b_energy(int i);
+  float sc_ftof_2_energy(int i);
+  float sc_ctof_energy(int i);
+  float sc_cnd_energy(int i);
   float ft_cal_energy(int i);
+  float ft_hodo_energy(int i);
+ 
  };
 #endif
